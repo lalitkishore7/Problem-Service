9f62b3260edbbbab64d4cf542e1fa7484bf4cf49
lalitkishore7 <kishorlalit230@gmail.com>
2026-06-12 20:07:37 +0530

    Feat: Add http-status-code library

--- src/controllers/problem.controller.js
+++ src/controllers/problem.controller.js
@@ -1,24 +1,38 @@
+const { StatusCodes } = require('http-status-codes')
+
 function problemPingCheck(req, res) {
     return res.json({message: "Problem controller service is alive"})
 }
 
 function addProblem(req, res) {
-    
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+        message: 'Not implemented'
+    })
 }
 
 function getProblem(req, res) {
-    
+     return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+       message: "Not implemented",
+     });
 }
 
 function getProblems(req, res) {
-
+    return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+    message: "Not implemented",
+    });
 }
 
 function updateProblem(req, res) {
-    
+     return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+       message: "Not implemented",
+     });
 }
 
-function deleteProblem(req, res) {}
+function deleteProblem(req, res) {
+     return res.status(StatusCodes.NOT_IMPLEMENTED).json({
+       message: "Not implemented",
+     });
+}
 
 module.exports = {
     addProblem,
